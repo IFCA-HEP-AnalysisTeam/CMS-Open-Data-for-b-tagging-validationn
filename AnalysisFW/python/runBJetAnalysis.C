24b44ea605a8b409f464adbf60bc7738e7364aed
Barbara <chazinb@ifca.unican.es>
2018-06-27 17:31:24 +0200

    update

--- AnalysisFW/python/runBJetAnalysis.C
+++ AnalysisFW/python/runBJetAnalysis.C
@@ -1,27 +1,41 @@
 #include "BJetAnalysis.C"
+#include "TSystem.h"
+#include "TChain.h"
 
-void runBJetAnalysis (TString filename, bool mctrue, TString rangeOfpt)
+void runBJetAnalysis (TString foldername, int mctrue, TString rangeOfpt)
 {
    //if (fChain == 0) return;
    //TString _dataPath = filename;
    bool    _ismc = mctrue;
-   cout << " _ismc " << _ismc << endl;
-   cout << " mctrue " << mctrue << endl;
-   //TString _ptRange = rangeOfpt;
-  // open the input file
-   TFile* infile  = new TFile( filename, "read" );
-  // get the tree
-   TTree* mytree  = (TTree*) infile -> Get( "ak5ak7/OpenDataTree");
-   Int_t nentries = (Int_t) mytree->GetEntries();
+  
+   // Read the tree from an input filename
+  // TFile* infile  = new TFile( filename, "read" );
+  // TTree* mytree  = (TTree*) infile -> Get( "ak5ak7/OpenDataTree");
+  // Int_t nentries = (Int_t) mytree->GetEntries();
+  // // call the analysis functions
+  // BJetAnalysis BJetAnalysis(mytree);  // remeber change BJetAnalysis.C , BJetAnalysis.h and Jet
+  // BJetAnalysis.Loop(filename, _ismc, rangeOfpt); 
+   
+  // Create a TChain to read several output_*.root trees in the foldername folder.
+  TChain* mychain("ak5ak7/OpenDataTree");
+  TString a = gSystem->GetFromPipe("ls " + foldername + "/output_*.root | wc -l"); 
+  int b = atoi( a );
+  for (int nfile=0; nfile < b; nfile++)
+   {
+    TString numb; 
+    numb += nfile;
+    mychain->Add(foldername + "output_" + numb + ".root"); 
+   }
+   Int_t nentries = (Int_t) mychain->GetEntries();
 
-   BJetAnalysis BJetAnalysis(mytree); 
-   BJetAnalysis.Loop(filename, mctrue, rangeOfpt); 
-   //BJetAnalysis.Loop(_dataPath, _ismc, _ptRange); 
+   BJetAnalysis BJetAnalysis(mychain); 
+   BJetAnalysis.Loop(foldername, _ismc, rangeOfpt); 
  }
 
 # ifndef __CINT__
 int main(int argc, char ** argv)
 {
+ printf("\n  ismc = 0 -> data; ismc = 1 -> mc  \n");
  if (argc < 4)
   { 
    printf("\n ./runBJetAnalysis <filename> <bool ismc> <samplePt>\n");
@@ -33,7 +47,7 @@ int main(int argc, char ** argv)
    printf("\n      ./runBJetAnalysis <filename> <bool ismc> <samplePt>\n");
    return -1;
   }
-  else if (argc == 4)  runBJetAnalysis(argv[1], argv[2], argv[3]);
+  else if (argc == 4)  runBJetAnalysis(argv[1], atoi(argv[2]), argv[3]);
   return 0;
 }
 #endif
